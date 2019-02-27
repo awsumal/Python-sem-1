5c42712f32b9b77ba399670358680bbd2c150e54
awsumal <47911700+awsumal@users.noreply.github.com>
2019-02-27 12:02:22 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+a={1:"com",2:"pu",3:"ter",'computer':55}
+print("\nDictionary is:\n")
+print(a)
+print("\nKeys are:\n")
+for i in a:
+	print(i)
+print("\nValues are:\n")
+for i in a:
+	print(a[i])
+print("\nItems are:\n")
+for x,y in a.items():
+	print(x,y)
---
+++ rpsfinal.py
@@ -0,0 +1,26 @@
+import random
+us,cs=0,0
+rps={'r':"rock",'p':"paper",'s':"scissor",'q':"quit"}
+psr=['r','p','s']
+while True:
+	user=input("\nEnter your choice, r for rock, p for paper, s for scissor, q to quit \n")
+	comp=random.choice(psr)
+	if user=='q':
+		print("\nFinal scores are \nuser:",us,"\ncomputer:",cs)
+		if(us>cs):
+			print("User wins")
+		elif(us==cs):
+			print("It is a draw")
+		else:
+			print("Computer wins")
+		exit()
+	print("\nYou have chosen",rps[user])
+	print("Computer has chosen",rps[comp])
+	if user==comp:
+		print("Draw")
+	elif (user=='r' and comp=='s') or (user=='p' and comp=='r') or (user=='s' and comp=='p'):
+		print("User gets a point")
+		us=us+1
+	else:
+		print("Computer gets a point")
+		cs=cs+1
